84d9e7b50722157181675d2ffedef7515347faac
meet <sitesabc091@gmail.com>
2024-12-23 06:31:06 +0000

    variables and datatypes

---
+++ 01_basics/01-variables.js
@@ -0,0 +1,10 @@
+const accountId = 121212    //constrants
+let accountEmail = "meet@gmail.com"     //variables which can change
+let accountState;
+
+var accountPass = "0909090" //optional for use var(variable) bcz of issue in block scope and functional scope
+
+accountCity = "surat"   //min. for use
+
+// console.log(accountId);
+console.table([accountId, accountEmail, accountPass, accountCity, accountState]);
---
+++ 01_basics/02-datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; //treat all js code as newer version
+// alert()
+
+let name = "meet"   //string datatypes..
+let age = 19   //num,integer
+let isLoggedIn = false   //boolean (true or false)
+//null (standalone value)empty value
+//undefined
+//symbol (unique)
+
+
+console.log(typeof isLoggedIn);
