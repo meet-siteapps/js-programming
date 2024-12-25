c9d0f837aa080f1fcfb575213198689fd8f78a5e
meet <sitesabc091@gmail.com>
2024-12-25 10:45:39 +0000

    compare data

---
+++ 01_basics/04-comparison.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+//11 thi 17 unusual che kyrey null ni value nan aave kyre 0 so use it very less before check
+
+// === 
+
+console.log("2" === 2);
