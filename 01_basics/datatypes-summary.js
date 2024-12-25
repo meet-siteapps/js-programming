faeeab7b920763576945056d9ee568cec57716a6
meet <sitesabc091@gmail.com>
2024-12-25 11:10:11 +0000

    change in location of file and revise datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,20 @@
+// Primitive datatypes
+    //7 types :string, number, boolean, null, undefined, symbol (is for unique), bigint(is for scientific and large value)
+
+// js is dynamic type or static type lang. ?
+    
+const score = 100           //num
+const scorevalue = 100.99   //number
+
+const isloggedin = false //simple boolean type
+const temperature = null //null type
+let userEmail;      //undefined type    or
+let userid = undefined      //undefined type
+
+const id = symbol("123")    //SYMBOL DATATYPE
+const id1 = symbol("123")
+console.log = (id === id1);
+
+
+//Reference datatpe (non Primitive)
+    //array, objects, functions
