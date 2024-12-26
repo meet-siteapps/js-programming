d210ff3680f0d03696533c938e1c76dac15bf9ff
meet <sitesabc091@gmail.com>
2024-12-26 04:16:26 +0000

    datatype-summary javascript

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -8,13 +8,35 @@ const scorevalue = 100.99   //number
 
 const isloggedin = false //simple boolean type
 const temperature = null //null type
-let userEmail;      //undefined type    or
-let userid = undefined      //undefined type
+let userEmail;      //undefined type
 
-const id = symbol("123")    //SYMBOL DATATYPE
-const id1 = symbol("123")
-console.log = (id === id1);
+const id = Symbol('123')     //symboldatatype
+const anotherId = Symbol('123')
+// console.log(id === anotherId);
+
+const bignum = 38493847534958373928n    //bigint datatype ...n
+// console.log(typeof(bignum));
 
 
 //Reference datatpe (non Primitive)
     //array, objects, functions
+
+const car = ["BMW", "Ferrari", "RR"]    //array
+console.log(car);
+
+
+let myobj = {               //object
+    name: "meet",
+    age: 19,
+}
+console.log(myobj);
+
+
+const myFunction = function(params) {
+    console.log("Hello World");
+}
+
+// for finding datatype
+console.log(typeof myFunction, typeof myobj, typeof car);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
